6d226db1eab63262d364ca93cbf7120034a3f545
Daniil Shliakhov <110263140+itshliakhov@users.noreply.github.com>
2023-02-09 15:47:55 +0200

    Updated Navigation.tsx
    Added some styles to nav and span to make navigation beautiful in mobile

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,11 +3,11 @@ import {Link} from 'react-router-dom';
 
 export function Navigation() {
     return (
-        <nav className={'flex justify-between items-center h-[50px] px-5 shadow-md bg-cyan-700 text-white h-[100px]'}>
+        <nav className={'flex xsm:flex-col md:flex-row  md:justify-between items-center h-[50px] px-5 shadow-md bg-cyan-700 text-white h-[120px]'}>
             <h3 className={'font-bold text-[40px]'}>Github Search</h3>
 
 
-            <span>
+            <span className={'xsm:mt-[15px]>
                 <Link to={'/'} className={'mr-8 font-bold'}>Home</Link>
                 <Link to={'/favourites'} className={'font-bold'}>Favourites</Link>
             </span>
