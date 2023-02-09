c2637194f69bbbb7d88ca51b7c221ef1ebd544e3
Daniil Shliakhov <110263140+itshliakhov@users.noreply.github.com>
2023-02-09 15:46:35 +0200

    Updated tailwind.config.js
    Created custom media query

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
-/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
     content: [
         './src/**/*.{js,jsx,ts,tsx}',
     ],
     theme: {
+        screens: {
+            'xsm': '320px',
+            ...defaultTheme.screens,
+        },
         extend: {},
     },
     plugins: [],
