576bf349686b1acb8ff2e96e73f3f34c6b4cc24f
Daniil Shliakhov <110263140+itshliakhov@users.noreply.github.com>
2023-02-26 13:09:45 +0200

    Update Navigation.tsx
    Made some style changes in nav tag

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom';
 
 export function Navigation() {
     return (
-        <nav className={'flex xsm:flex-col md:flex-row  md:justify-between items-center h-[50px] px-5 shadow-md bg-cyan-700 text-white h-[120px]'}>
+        <nav className={'flex xsm:flex-col md:flex-row  md:justify-between items-center px-5 shadow-md bg-cyan-700 text-white h-[120px]'}>
             <h3 className={'font-bold text-[40px]'}>Github Search</h3>
 
 
